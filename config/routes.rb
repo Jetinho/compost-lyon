ac513bdbcb050e7a7d146978cbfb36978f26e9d6
Julien Jet <jet.julien@gmail.com>
2020-11-01 09:16:40 +0100

    change sites url name

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :sites do
-  collection do
-    get 'search'
+  scope(path_names: { new: 'creation', edit: 'edition', search: 'rechercher' }) do
+    resources :sites, path: 'composteurs-collectifs' do
+      collection do
+        get 'search'#, as: 'sites/search'
+      end
+    end
   end
-end
 end
