45a93b00fe6c0dd675f696d7fb85f8d6be929fd9
Julien Jet <jet.julien@gmail.com>
2020-11-04 18:18:00 +0100

    add data to organisations

--- db/migrate/20201104164534_add_description_to_organisations.rb
+++ db/migrate/20201104164534_add_description_to_organisations.rb
@@ -1,6 +1,20 @@
 class AddDescriptionToOrganisations < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :organisations, :description, :text
     add_column :organisations, :meta_description, :string
+    eisenia = Organisation.find_by(slug: :eisenia)
+    eisenia.update(description: "Installation et maintenance de lombricomposteurs collectifs à Lyon métropole, sensibilisation et éducation à l'écologie")
+    grand_lyon_metropole = Organisation.find_by(slug: :grand_lyon_metropole)
+    grand_lyon_metropole.update(
+      description: "Pour développer la pratique du compostage et réduire la quantité de déchets dans les poubelles des habitants, la Métropole de Lyon accompagne le déploiement de composteurs partagés sur son territoire.",
+      slug: "grand-lyon-metropole"
+    )
+  end
+
+  def down
+    remove_column :organisations, :description, :text
+    remove_column :organisations, :meta_description, :string
+    grand_lyon_metropole = Organisation.find_by(slug: 'grand-lyon-metropole')
+    grand_lyon_metropole.update(slug: :grand_lyon_metropole)
   end
 end
