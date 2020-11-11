f6413c89c583c7e241ff9dab61168dd8afadc5ec
Julien Jet <jet.julien@gmail.com>
2020-11-11 23:51:57 +0100

    introduce super_admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,15 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-
     user ||= User.new # guest user (not logged in)
-    can :read, [Organisation, Site]
-    can :update, Organisation, user_id: user.id
-    can :update, Site do |site|
-      site.admin_id == user.id
+    if user.super_admin?
+      can :manage, :all
+    else
+      can :read, [Organisation, Site]
+      can :update, Organisation, user_id: user.id
+      can :update, Site do |site|
+        site.admin_id == user.id
+      end
     end
 
 
