71aadde3d61e8168218d58373f7d1d224253c75d
Julien Jet <jet.julien@gmail.com>
2021-02-06 16:57:53 +0100

    enable use of erb in js files (#49)

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,5 @@
 const { environment } = require('@rails/webpacker')
+const erb = require('./loaders/erb')
 
 const webpack = require('webpack');
 // Preventing Babel from transpiling NodeModules packages
@@ -11,4 +12,5 @@ environment.plugins.prepend('Provide',
     Popper: ['popper.js', 'default']
   })
 );
+environment.loaders.prepend('erb', erb)
 module.exports = environment
