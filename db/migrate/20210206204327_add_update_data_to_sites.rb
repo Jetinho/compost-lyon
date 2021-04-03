4d9e1f66de7afafe70fe34fb3230dea7e875a01e
Julien Jet <julien@neocamino.com>
2021-04-03 19:29:42 +0200

    #53: handle data update outside of migration

--- db/migrate/20210206204327_add_update_data_to_sites.rb
+++ db/migrate/20210206204327_add_update_data_to_sites.rb
@@ -2,7 +2,6 @@ class AddUpdateDataToSites < ActiveRecord::Migration[6.0]
   def up
     add_column :sites, :json_api_data, :text
     add_column :sites, :api_updated_at, :datetime
-    GrandLyonSitesUpdater.new.update_grand_lyon_composting_sites
   end
   def down
     remove_column :sites, :json_api_data
