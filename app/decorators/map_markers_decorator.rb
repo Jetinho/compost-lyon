8afc2a826b79360ead1267b1448b64797787f309
Julien Jet <julien@neocamino.com>
2021-05-03 11:22:32 +0200

    clean PR

--- app/decorators/map_markers_decorator.rb
+++ app/decorators/map_markers_decorator.rb
@@ -11,5 +11,4 @@ class MapMarkersDecorator < Draper::CollectionDecorator
   def to_map_marker_json
     to_json(only: [:id, :latitude, :longitude, :public], methods: [:map_marker_display])
   end
-
 end
