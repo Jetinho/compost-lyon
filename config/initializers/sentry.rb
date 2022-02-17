de36aca5fe32a88219c328a28b8645bdd5499332
Julien Jet <jet.julien@gmail.com>
2022-02-17 22:41:09 +0100

    Fix sentry settings (#93)
    * Disable Sentry tracing stuff when not in production
    
    * reduce sentry sample rate for transactions
    
    Co-authored-by: Thierry Jet <thierry.jet@kombicode.com>

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,9 +5,9 @@ Sentry.init do |config|
   # Set tracesSampleRate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production
-  config.traces_sample_rate = 0.5
-  # or
-  config.traces_sampler = lambda do |context|
-    true
+  if Rails.env.production?
+    config.traces_sample_rate = 0.2
+  else
+    config.traces_sample_rate = 0
   end
 end
